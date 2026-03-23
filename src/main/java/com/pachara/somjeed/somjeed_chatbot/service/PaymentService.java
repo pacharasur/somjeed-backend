df5d4c659de3833a369a08168723137ec3ca46ad
pacharasur <94338617+pacharasur@users.noreply.github.com>
2026-03-23 16:49:22 +0700

    remove unused code

--- src/main/java/com/pachara/somjeed/somjeed_chatbot/service/PaymentService.java
+++
@@ -1,8 +0,0 @@
-package com.pachara.somjeed.somjeed_chatbot.service;
-
-import com.pachara.somjeed.somjeed_chatbot.model.domain.ChatContext;
-
-public interface PaymentService {
-
-    ChatContext getChatContext();
-}
